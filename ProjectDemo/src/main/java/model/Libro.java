9e1d92077c544c39af0098ca35e87934da4ae99c
jael2555 <jael2555@gmail.com>
2022-04-10 14:49:47 -0300

    atributos y constructor

---
+++ ProjectDemo/src/main/java/model/Libro.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Libro {
+
+	private String titulo;
+	private String genero;
+	
+	public Libro() {
+		
+	}
+
+	public Libro(String titulo, String genero) {
+		super();
+		this.titulo = titulo;
+		this.genero = genero;
+	}
+	
+	
+}
